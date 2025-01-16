0ce5fd0fca0a192261137858c0ca9ecfe1bb7721
SirkoKropyvnytskyi <gorbatuk46566@gmail.com]>
2025-01-16 09:58:22 +0200

    Avoid modifying the original object by creating a new result object

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -2,13 +2,13 @@
 // Reverse dict, exchange keys and values
 
 const reverse = (data) => {
+  const result = {};
   const keys = Object.keys(data);
   keys.forEach((key) => {
     const value = data[key];
-    data[value] = key;
-    delete data[key];
+    result[value] = key;
   });
-  return data;
+  return result;
 };
 
 module.exports = reverse;
